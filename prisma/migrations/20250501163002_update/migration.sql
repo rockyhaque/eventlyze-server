eec530358939d2dee7b1684485ea858b9fdb347d
Rasibul <rasibul179@gmail.com>
2025-05-01 22:38:06 +0600

    updated

--- prisma/migrations/20250501163002_update/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "users" DROP CONSTRAINT "users_reviewId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Review" ADD CONSTRAINT "Review_id_fkey" FOREIGN KEY ("id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
