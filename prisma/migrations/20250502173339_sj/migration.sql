73eb6389b3d79e4910deedeb94ebb76534f08f02
Md Sakib al Hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-05-02 23:42:59 +0600

    fix:event servicers chagne

--- prisma/migrations/20250502173339_sj/migration.sql
+++ prisma/migrations/20250502173339_sj/migration.sql
@@ -33,7 +33,6 @@ CREATE TABLE "users" (
     "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "reviewId" TEXT,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
@@ -50,7 +49,6 @@ CREATE TABLE "subscribers" (
 -- CreateTable
 CREATE TABLE "events" (
     "id" TEXT NOT NULL,
-    "ownerId" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "isPublic" BOOLEAN NOT NULL DEFAULT false,
@@ -58,15 +56,23 @@ CREATE TABLE "events" (
     "price" INTEGER NOT NULL,
     "category" TEXT NOT NULL,
     "location" TEXT,
+    "platform" TEXT,
+    "meetingLink" TEXT,
+    "meetingLinkPassword" TEXT,
     "registrationStart" TIMESTAMP(3) NOT NULL,
     "registrationEnd" TIMESTAMP(3) NOT NULL,
     "eventStartTime" TIMESTAMP(3) NOT NULL,
     "eventEndTime" TIMESTAMP(3) NOT NULL,
     "seat" INTEGER NOT NULL,
+    "eventBanner" TEXT,
     "status" "EventStatus" NOT NULL DEFAULT 'UPCOMING',
     "eventType" "EventType" NOT NULL DEFAULT 'OFFLINE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
     "paymentId" TEXT,
     "reviewId" TEXT,
+    "ownerId" TEXT NOT NULL,
+    "inviteId" TEXT,
 
     CONSTRAINT "events_pkey" PRIMARY KEY ("id")
 );
@@ -85,9 +91,7 @@ CREATE TABLE "participants" (
 -- CreateTable
 CREATE TABLE "Invite" (
     "id" TEXT NOT NULL,
-    "eventId" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "inviteId" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Invite_pkey" PRIMARY KEY ("id")
 );
@@ -95,7 +99,6 @@ CREATE TABLE "Invite" (
 -- CreateTable
 CREATE TABLE "Payment" (
     "paymentId" TEXT NOT NULL,
-    "eventId" TEXT NOT NULL,
     "userId" TEXT,
     "paymentUrl" TEXT NOT NULL,
     "status" "PaymentStatus" NOT NULL DEFAULT 'PENDING',
@@ -109,7 +112,6 @@ CREATE TABLE "Payment" (
 -- CreateTable
 CREATE TABLE "Review" (
     "id" TEXT NOT NULL,
-    "eventId" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "rating" INTEGER NOT NULL,
@@ -119,30 +121,11 @@ CREATE TABLE "Review" (
     CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "_EventParticipants" (
-    "A" TEXT NOT NULL,
-    "B" TEXT NOT NULL,
-
-    CONSTRAINT "_EventParticipants_AB_pkey" PRIMARY KEY ("A","B")
-);
-
--- CreateTable
-CREATE TABLE "_EventInvites" (
-    "A" TEXT NOT NULL,
-    "B" TEXT NOT NULL,
-
-    CONSTRAINT "_EventInvites_AB_pkey" PRIMARY KEY ("A","B")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
--- CreateIndex
-CREATE INDEX "_EventParticipants_B_index" ON "_EventParticipants"("B");
-
--- CreateIndex
-CREATE INDEX "_EventInvites_B_index" ON "_EventInvites"("B");
+-- AddForeignKey
+ALTER TABLE "events" ADD CONSTRAINT "events_inviteId_fkey" FOREIGN KEY ("inviteId") REFERENCES "Invite"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "events" ADD CONSTRAINT "events_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -160,22 +143,7 @@ ALTER TABLE "participants" ADD CONSTRAINT "participants_eventId_fkey" FOREIGN KE
 ALTER TABLE "participants" ADD CONSTRAINT "participants_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Invite" ADD CONSTRAINT "Invite_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "Payment" ADD CONSTRAINT "Payment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Review" ADD CONSTRAINT "Review_id_fkey" FOREIGN KEY ("id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_EventParticipants" ADD CONSTRAINT "_EventParticipants_A_fkey" FOREIGN KEY ("A") REFERENCES "events"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_EventParticipants" ADD CONSTRAINT "_EventParticipants_B_fkey" FOREIGN KEY ("B") REFERENCES "participants"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_EventInvites" ADD CONSTRAINT "_EventInvites_A_fkey" FOREIGN KEY ("A") REFERENCES "events"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_EventInvites" ADD CONSTRAINT "_EventInvites_B_fkey" FOREIGN KEY ("B") REFERENCES "Invite"("id") ON DELETE CASCADE ON UPDATE CASCADE;
