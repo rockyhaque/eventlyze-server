91b8c3f608ddf3a0cbc9fb826298bf6493ce7368
FARUK2059 <omarfaruk34552@gmail.com>
2025-05-03 15:02:11 +0600

    feat: add user and admin all notifiaction route

--- src/app/modules/Notification/notification.route.ts
+++ src/app/modules/Notification/notification.route.ts
@@ -4,9 +4,25 @@ import { NotificationController } from './notification.controller';
 
 const router = express.Router();
 
+
+// Create Notification Route
 router.post(
     "/create-notification",
     NotificationController.addNotification
+);
+
+
+// All Notification Route By Admin
+router.get(
+    "/admin-notification",
+    NotificationController.allNotificationByAdmin
+);
+
+
+// All Notification Route By User
+router.get(
+    "/user-notification",
+    NotificationController.allNotificationByUser
 );
 
 
