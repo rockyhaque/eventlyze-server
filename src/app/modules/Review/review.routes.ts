84f26961c7c71f7f2ecaeffec0b873ce96199423
unknown <ranaot56@gmail.com>
2025-05-03 21:37:08 +0600

    feat:Create review module

--- src/app/modules/Review/review.routes.ts
+++ src/app/modules/Review/review.routes.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import { ReviewController } from "./review.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
-router.get("/:eventId", ReviewController.getReviews);
+router.get("/",auth(UserRole.USER), ReviewController.getReviewsByUserId);
 
-router.post("/:eventId", ReviewController.createReview);
+router.post("/",auth(UserRole.USER), ReviewController.createReview);
 
-router.put("/:reviewId", ReviewController.updateReview);
+router.put("/:reviewId",auth(UserRole.USER), ReviewController.updateReview);
 
 router.delete("/:reviewId", ReviewController.deleteReview);
 
