2c6026b2f3944cd19f16d4447b502e701cbee39a
FARUK2059 <omarfaruk34552@gmail.com>
2025-05-04 22:58:04 +0600

    feat: new test add event and notification create test

--- src/app/modules/Event/event.service.ts
+++ src/app/modules/Event/event.service.ts
@@ -10,140 +10,181 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 
-const createEvent = async (data:Event, user:JwtPayload) => {
-    
-    const email = user?.email;
-    const existingUser = await prisma.user.findUnique({
-        where: { email },
-        select: { id: true },
+// const createEvent = async (data:Event, user:JwtPayload) => {
+
+//     const email = user?.email;
+//     const existingUser = await prisma.user.findUnique({
+//         where: { email },
+//         select: { id: true },
+//     });
+
+//     if (!existingUser) {
+//         throw new AppError(StatusCodes.NOT_FOUND, 'User not found');
+//     }
+
+//     const event = await prisma.event.create({
+//         data: {
+//             ...data,
+//             ownerId: existingUser.id,
+//         },
+//     })
+
+//     return event;
+// };
+
+
+
+const createEvent = async (data: Event, user: JwtPayload) => {
+
+  const email = user?.email;
+  const existingUser = await prisma.user.findUnique({
+    where: { email },
+    select: { id: true },
+  });
+
+  if (!existingUser) {
+    throw new AppError(StatusCodes.NOT_FOUND, 'User not found');
+  }
+
+
+  const result = await prisma.$transaction(async (transactionClient: Prisma.TransactionClient) => {
+
+   const event = await transactionClient.event.create({
+      data: {
+        ...data,
+        ownerId: existingUser.id,
+      },
     });
 
-    if (!existingUser) {
-        throw new AppError(StatusCodes.NOT_FOUND, 'User not found');
-    }
+    console.log(event);
     
-    const event = await prisma.event.create({
-        data: {
-            ...data,
-            ownerId: existingUser.id,
-        },
-    })
+
+     await transactionClient.notification.create({
+      data: {
+        userId: existingUser?.id,
+        eventId: event?.id,
+        message: `New ${event?.title} event created by ${email}`
+      }
+    });
 
     return event;
+  });
+
+  return result;
 };
 
 
 
 const parseBoolean = (value: string | undefined): boolean | undefined => {
-    if (value === 'true') return true;
-    if (value === 'false') return false;
-    return undefined;
+  if (value === 'true') return true;
+  if (value === 'false') return false;
+  return undefined;
 };
 
 
-const getAllEvents = async (params:any,options:any) => {
- // console.log(options)
- const { page, limit, skip } = paginationHelper.calculatePagination(options);
- const { searchTerm, ...filterData } = params;
- const andConditions: Prisma.EventWhereInput[] = [];
+const getAllEvents = async (params: any, options: any) => {
+  // console.log(options)
+  const { page, limit, skip } = paginationHelper.calculatePagination(options);
+  const { searchTerm, ...filterData } = params;
+  const andConditions: Prisma.EventWhereInput[] = [];
 
- if (params.searchTerm) {
-   andConditions.push({
-     OR:eventSearchAbleFields.map((field) => ({
-       [field]: {
-         contains: params.searchTerm,
-         mode: "insensitive",
-       },
-     })),
-   });
- }
+  if (params.searchTerm) {
+    andConditions.push({
+      OR: eventSearchAbleFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
 
- if(Object.keys(filterData).length > 0) {
+  if (Object.keys(filterData).length > 0) {
 
- }
+  }
 
- //   For spesific field filter
-if (Object.keys(filterData).length > 0) {
-  andConditions.push({
-    AND: Object.keys(filterData).map((key) => {
-      let value = (filterData as any)[key];
+  //   For spesific field filter
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => {
+        let value = (filterData as any)[key];
 
-     
-      if (value === "true") value = true;
-      if (value === "false") value = false;
 
-      if (typeof value === "boolean") {
-        return { [key]: value }; 
-      } else {
-        return { [key]: { equals: value } };
-      }
-    }),
+        if (value === "true") value = true;
+        if (value === "false") value = false;
+
+        if (typeof value === "boolean") {
+          return { [key]: value };
+        } else {
+          return { [key]: { equals: value } };
+        }
+      }),
+    });
+  }
+
+
+  const whereConditions: Prisma.EventWhereInput = andConditions.length > 0 ? { AND: andConditions } : {};
+
+  console.dir(whereConditions, { depth: null })
+
+  const result = await prisma.Event.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+          [options.sortBy]: options.sortOrder,
+        }
+        : {
+          createdAt: "desc",
+        },
   });
-}
 
+  const total = await prisma.Event.count({
+    where: whereConditions,
+  });
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
 
- const whereConditions: Prisma.EventWhereInput =andConditions.length > 0 ? { AND: andConditions } : {};
- 
- console.dir(whereConditions,{ depth: null })
- 
- const result = await prisma.Event.findMany({
-   where: whereConditions,
-   skip,
-   take: limit,
-   orderBy:
-     options.sortBy && options.sortOrder
-       ? {
-           [options.sortBy]: options.sortOrder,
-         }
-       : {
-           createdAt: "desc",
-         },
- });
-
- const total = await prisma.Event.count({
-   where: whereConditions,
- });
- return {
-   meta: {
-     page,
-     limit,
-     total,
-   },
-   data: result,
- };
-  
 }
 
 const getEventById = async (id: string) => {
-    const event = await prisma.event.findUnique({
-        where: { id },
+  const event = await prisma.event.findUnique({
+    where: { id },
 
-    });
-    return event;
+  });
+  return event;
 };
 
-const updateSingleEvent = async (id: string, data:Partial<Event>) => {
-    const event = await prisma.event.update({
-        where: { id },
-        data,
-    });
-    return event;
+const updateSingleEvent = async (id: string, data: Partial<Event>) => {
+  const event = await prisma.event.update({
+    where: { id },
+    data,
+  });
+  return event;
 };
 
 const deleteSingleEvent = async (id: string) => {
-    const event = await prisma.event.delete({
-        where: { id },
-    });
-    return event;
+  const event = await prisma.event.delete({
+    where: { id },
+  });
+  return event;
 };
 
 
 
 export const eventService = {
-    createEvent,
-    getAllEvents,
-    getEventById,
-    updateSingleEvent,
-    deleteSingleEvent,
+  createEvent,
+  getAllEvents,
+  getEventById,
+  updateSingleEvent,
+  deleteSingleEvent,
 
 };
