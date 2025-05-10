beee5fdda217c2d3b88161a0cd58de56cc8eba1a
Rocky Haque <rockyhaque99@gmail.com>
2025-05-11 00:14:03 +0600

    fix: typo fix

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -1,36 +1,41 @@
 import { StatusCodes } from "http-status-codes";
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
-import { PaymentService } from "./payment.service";
 import { Request, Response } from "express";
 import { TAuthUser } from "../../interfaces/common";
-
+import { PaymentService } from "./payment.service";
 
 interface CustomRequest extends Request {
   user?: any;
 }
 
-const createPayment = catchAsync(async (req: Request & { user?: TAuthUser }, res: Response) => {
-  const result = await PaymentService.createPayment(req.body, req.user as TAuthUser);
-  sendResponse(res, {
-    statusCode: StatusCodes.OK,
-    success: true,
-    message: "successfully get payment",
-    data: result,
-  });
-});
+const createPayment = catchAsync(
+  async (req: Request & { user?: TAuthUser }, res: Response) => {
+    const result = await PaymentService.createPayment(
+      req.body,
+      req.user as TAuthUser
+    );
+    sendResponse(res, {
+      statusCode: StatusCodes.OK,
+      success: true,
+      message: "successfully get payment",
+      data: result,
+    });
+  }
+);
 
-
-const validatePayment = catchAsync(async (req: CustomRequest, res: Response) => {
-  const users = req.user as TAuthUser;
-  const result = await PaymentService.validatePayment(req.query, users);
-  sendResponse(res, {
-    statusCode: StatusCodes.OK,
-    success: true,
-    message: 'Payment validate successfully',
-    data: result,
-  });
-});
+const validatePayment = catchAsync(
+  async (req: CustomRequest, res: Response) => {
+    const users = req.user as TAuthUser;
+    const result = await PaymentService.validatePayment(req.query, users);
+    sendResponse(res, {
+      statusCode: StatusCodes.OK,
+      success: true,
+      message: "Payment validate successfully",
+      data: result,
+    });
+  }
+);
 
 const getPayment = catchAsync(async (req: Request, res: Response) => {
   const { userId, eventId } = req.params;
@@ -43,22 +48,21 @@ const getPayment = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
-const paymentSuccess = catchAsync(async (req:Request, res:Response) => {
-  const {id} = req.params;
-  const result = await PaymentService.paymentSuccess(id)  ;
+const paymentSuccess = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await PaymentService.paymentSuccess(id);
   res.redirect(301, result as string);
 });
 
-const paymentFailed = catchAsync(async (req:Request, res:Response) => {
-  const {id} = req.params;
-  const result = await PaymentService.paymentFailed(id)  ;
+const paymentFailed = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await PaymentService.paymentFailed(id);
   res.redirect(301, result as string);
 });
 
-
-const paymentCancel = catchAsync(async (req:Request, res:Response) => {
-  const {id} = req.params;
-  const result = await PaymentService.paymentCancel(id)  ;
+const paymentCancel = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await PaymentService.paymentCancel(id);
   res.redirect(301, result as string);
 });
 
@@ -68,5 +72,5 @@ export const PaymentController = {
   validatePayment,
   paymentSuccess,
   paymentFailed,
-  paymentCancel
+  paymentCancel,
 };
