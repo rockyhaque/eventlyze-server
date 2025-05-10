c6daf46b00031ec088405c072711f230e81e487a
Rashed <rashed@notionhive.com>
2025-05-10 21:36:31 +0600

    fix: payment error

--- src/app/modules/payment/payment.routes.ts
+++ src/app/modules/payment/payment.routes.ts
@@ -4,7 +4,10 @@ import { UserRole } from "@prisma/client";
 import { PaymentController } from "./payment.controller";
 
 const router = express.Router();
-router.get("/status/:eventId/:userId", PaymentController.getpayment);
+router.get("/status/:eventId/:userId", PaymentController.getPayment);
+router.post("/success/:id",PaymentController.paymentSuccess);
+router.post("/failed/:id",PaymentController.paymentFailed);
+router.post("/cancel/:id",PaymentController.paymentCancel);
 router.post("/create", auth(UserRole.USER), PaymentController.createPayment);
 router.get(
     '/ipn',
